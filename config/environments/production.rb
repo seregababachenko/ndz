240dc2999454ed18379195966f7c8d03f071fee4
seregababachenko <31434413+seregababachenko@users.noreply.github.com>
2017-10-14 13:01:10 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.asset_host = ""
-  config.action_mailer.default_url_options = { :host => '' }
+  config.action_mailer.asset_host = "nashdom.zp.ua"
+  config.action_mailer.default_url_options = { :host => 'nashdom.zp.ua' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => '',
@@ -86,7 +86,7 @@ Rails.application.configure do
       :password             => '',
       :authentication       => 'plain',
       :enable_starttls_auto => true,
-      :domain               => ''
+      :domain               => 'nashdom.zp.ua'
   }
 
 end
